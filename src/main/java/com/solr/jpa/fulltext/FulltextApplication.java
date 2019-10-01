280149cf82d4657c7e3c0ca774da7a07599727dd
goaulda <sabat.adam@gmail.com>
2019-10-01 12:27:51 +0200

    Init

---
+++ src/main/java/com/solr/jpa/fulltext/FulltextApplication.java
@@ -0,0 +1,13 @@
+package com.solr.jpa.fulltext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulltextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FulltextApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solr/jpa/fulltext/FulltextApplicationTests.java
@@ -0,0 +1,16 @@
+package com.solr.jpa.fulltext;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FulltextApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
